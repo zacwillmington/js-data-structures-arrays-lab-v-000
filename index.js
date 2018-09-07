5df3026c8fc255cd3849216104ec8d5c9f119057
Zac Willmington <zac.willmington@gmail.com>
2018-09-07 20:04:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function destructivelyRemoveFirstDriver(){
 function appendDriver(name){
 
     const addedDriver = [ ...drivers.slice(0, drivers.length - 1), name];
+    
     console.log(addedDriver);
-    // console.log(drivers);
     console.log(drivers);
 }
